021cb0d2d98e371d2537d80a1acf50d7cf81faf6
Lorenzo Macias <lzomacias@gmail.com>
2025-02-24 10:49:52 -0500

    edited editProfil app.jsx signup login navbar

--- capstone-fe/src/components/Header.jsx
+++ capstone-fe/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <nav>
+        <Link to ="/"> Home </Link>
+        <Link to ="/myprofile"> Profile </Link>
+        <Link to ="/communities"> Communities </Link>
+        <Link to ="/messages"> Messages </Link>
+    </nav>
+  )
 }
 
 export default Header;

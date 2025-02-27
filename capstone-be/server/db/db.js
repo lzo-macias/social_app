04348774b5b5cb63800adfb4e9d921a88695ba14
Tristan <119907414+plots-TH@users.noreply.github.com>
2025-02-27 13:53:19 -0500

    combined personal post and community post tables into a single posts table

--- capstone-be/server/db/db.js
+++ capstone-be/server/db/db.js
@@ -8,11 +8,10 @@ const createTables = async () => {
 
       DROP TABLE IF EXISTS messages CASCADE;
       DROP TABLE IF EXISTS posts CASCADE;
-      DROP TABLE IF EXISTS personal_posts;
       DROP TABLE IF EXISTS community_members CASCADE;
       DROP TABLE IF EXISTS users CASCADE;
       DROP TABLE IF EXISTS communities CASCADE;
-      DROP TABLE IF EXISTS images;
+      DROP TABLE IF EXISTS images CASCADE;
 
       CREATE TABLE images (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -58,16 +57,8 @@ const createTables = async () => {
       CREATE TABLE posts (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
         user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-        community_id UUID REFERENCES communities(id) ON DELETE CASCADE,
+        community_id UUID REFERENCES communities(id) ON DELETE CASCADE, -- Nullable for profile posts
         title VARCHAR(255),
-        content TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
-
-      CREATE TABLE personal_posts (
-        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        user_id UUID REFERENCES users(id) ON DELETE CASCADE,
         content TEXT NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- capstone-be/server/db/personalPost.js
+++ capstone-be/server/db/personalPost.js
@@ -2,69 +2,68 @@ const { pool } = require("./index"); // Import client from the db setup
 const { v4: uuidv4 } = require("uuid");
 
 const createPersonalPost = async ({ userId, content }) => {
-    try {
-      const SQL = `
+  try {
+    const SQL = `
           INSERT INTO personal_posts(id, user_id, content)
           VALUES($1, $2, $3) RETURNING *;
         `;
-      const { rows } = await pool.query(SQL, [
-        uuidv4(),
-        userId,
-        content,
-      ]);
-      return rows[0];
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const { rows } = await pool.query(SQL, [uuidv4(), userId, content]);
+    return rows[0];
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-  const UpdatePersonalPost = async ({ postId, content }) => {
-    try {
-      const SQL = `
-          UPDATE personal_posts SET content =$1 WHERE id = $2
+const UpdatePersonalPost = async ({ postId, content }) => {
+  try {
+    const SQL = `
+          UPDATE posts SET content =$1 WHERE id = $2
           RETURNING *;
         `;
-      const { rows } = await pool.query(SQL, [
-        content,postId
-      ]);
-      return rows[0];
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  
-  const fetchPostsByUser = async (userId) => {
-    try {
-      const SQL = `SELECT * FROM personal_posts WHERE user_id = $1 ;`;
-      const { rows } = await pool.query(SQL, [userId]);
-      return rows;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  };
+    const { rows } = await pool.query(SQL, [content, postId]);
+    return rows[0];
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-  const fetchPostbyId = async (postId) => {
-    try {
-      const SQL = `SELECT * FROM personal_posts WHERE id = $1 ;`;
-      const { rows } = await pool.query(SQL, [postId]);
-      return rows[0];
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  };
-  
+const fetchPostsByUser = async (userId) => {
+  try {
+    const SQL = `SELECT * FROM posts WHERE user_id = $1 ;`;
+    const { rows } = await pool.query(SQL, [userId]);
+    return rows;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
-  const deletePersonalPost = async (personal_postId) => {
-    try {
-      const SQL = `DELETE FROM personal_posts WHERE id = $1 RETURNING *;`;
-      const { rows } = await pool.query(SQL, [personal_postId]);
-      return rows[0];
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  };
-  
-  module.exports = {createPersonalPost,fetchPostsByUser,fetchPostbyId,deletePersonalPost,UpdatePersonalPost }
+const fetchPostbyId = async (postId) => {
+  try {
+    const SQL = `SELECT * FROM posts WHERE id = $1 ;`;
+    const { rows } = await pool.query(SQL, [postId]);
+    return rows[0];
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const deletePersonalPost = async (personal_postId) => {
+  try {
+    const SQL = `DELETE FROM posts WHERE id = $1 RETURNING *;`;
+    const { rows } = await pool.query(SQL, [personal_postId]);
+    return rows[0];
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+module.exports = {
+  createPersonalPost,
+  fetchPostsByUser,
+  fetchPostbyId,
+  deletePersonalPost,
+  UpdatePersonalPost,
+};

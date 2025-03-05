cb1ae29041c86532c72256b1cd0001102f4e3d30
Tristan <119907414+plots-TH@users.noreply.github.com>
2025-03-04 21:24:32 -0500

    added comments to community posts

--- capstone-be/server/api/communityPostCommentRoutes.js
+++ capstone-be/server/api/communityPostCommentRoutes.js
@@ -2,25 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 const {
-fetchCommentsByPostCommunity,
-createCommunityPostComment,
-updateCommunityPostComment,
-deleteCommunityPostComment} = require("../db/communityPostComments")
+  fetchCommentsByPostCommunity,
+  createCommunityPostComment,
+  updateCommunityPostComment,
+  deleteCommunityPostComment,
+} = require("../db/communityPostComments");
 
 const isLoggedIn = require("../middleware/isLoggedIn");
-const { pool } = require("../db/index"); 
+const { pool } = require("../db/index");
 
 //Community post comment routes
 
-//Post comment to community post
-
+// POST: Create a comment for a community post
 router.post(
-  "/:communityId/:postId/comment", isLoggedIn,
-
+  "/:communityId/:postId/comment",
+  isLoggedIn,
   async (req, res, next) => {
     try {
-      const {postId,communityId} = req.params;
-      const createdbyId = req.user;
+      const { postId, communityId } = req.params;
+      // Use req.user.id instead of req.user
+      const createdbyId = req.user.id;
       const { comment } = req.body;
 
       if (!comment) {
@@ -45,7 +46,7 @@ router.post(
 // Gets all comments for a specific post
 router.get("/:communityId/:postId/comments", isLoggedIn, async (req, res) => {
   try {
-    const { postId, communityId} = req.params;
+    const { postId, communityId } = req.params;
     const communityPostComments = await fetchCommentsByPostCommunity(postId);
     res.status(200).json(communityPostComments);
   } catch (err) {
@@ -55,35 +56,43 @@ router.get("/:communityId/:postId/comments", isLoggedIn, async (req, res) => {
 });
 
 // Updates a comment for a specific post
-router.put("/:communityId/:postId/:commentId", isLoggedIn, async (req, res, next) => {
-  try {
-    const { commentId } = req.params;
-    const { comment } = req.body;
-    const result = await updateCommunityPostComment( commentId, comment );
+router.put(
+  "/:communityId/:postId/:commentId",
+  isLoggedIn,
+  async (req, res, next) => {
+    try {
+      const { commentId } = req.params;
+      const { comment } = req.body;
+      const result = await updateCommunityPostComment(commentId, comment);
 
-    console.log("Comment updated!");
-    res.status(200).json(result); // Send the updated profile
-  } catch (err) {
-    console.error("Error in PUT :communityId/:postId/:commentId", err);
-    next(err); // Forward error to error handler
+      console.log("Comment updated!");
+      res.status(200).json(result); // Send the updated profile
+    } catch (err) {
+      console.error("Error in PUT :communityId/:postId/:commentId", err);
+      next(err); // Forward error to error handler
+    }
   }
-});
+);
 
 // Deletes personal post
-router.delete("/:communityId/:postId/:commentId", isLoggedIn, async (req, res) => {
-  const { commentId } = req.params;
-  try {
-    const deletedComment = await deleteCommunityPostComment(commentId);
+router.delete(
+  "/:communityId/:postId/:commentId",
+  isLoggedIn,
+  async (req, res) => {
+    const { commentId } = req.params;
+    try {
+      const deletedComment = await deleteCommunityPostComment(commentId);
 
-    if (!deletedComment) {
-      return res.status(404).json({ error: "Unable to delete comment" });
+      if (!deletedComment) {
+        return res.status(404).json({ error: "Unable to delete comment" });
+      }
+
+      res.status(200).json({ message: "comment deleted", deletedComment });
+    } catch (err) {
+      console.error("Error in DELETE /:postId/:commentId:", err);
+      res.status(500).json({ error: "Failed to delete comment" });
     }
-
-    res.status(200).json({ message: "comment deleted", deletedComment });
-  } catch (err) {
-    console.error("Error in DELETE /:postId/:commentId:", err);
-    res.status(500).json({ error: "Failed to delete comment" });
   }
-});
+);
 
 module.exports = router;

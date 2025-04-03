e2474a7bc5384de2d718685a7c73a870ddad301e
Lorenzo Macias <lzomacias@gmail.com>
2025-04-03 19:41:05 -0400

    bugs, not the cleanest but working search bar, this will be done by EOD friday =)

--- capstone-fe/src/pages/Home.jsx
+++ capstone-fe/src/pages/Home.jsx
@@ -3,9 +3,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-function Home() {
+function Home({ searchTerm }) {
   const [posts, setPosts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
 
 
